ae3859d85ae7f97901c996b406e5b753a05d296e
yf <291323003@qq.com>
2019-05-19 23:15:48 +0800

    up fix

--- src/WorkerProcess.php
+++ src/WorkerProcess.php
@@ -5,13 +5,18 @@ namespace EasySwoole\Rpc;
 
 
 use EasySwoole\Component\Process\AbstractProcess;
+use Swoole\Coroutine\Socket;
 
 class WorkerProcess extends AbstractProcess
 {
 
     public function run($arg)
     {
-        // TODO: Implement run() method.
+        $socket = new Socket(AF_INET,SOCK_STREAM,0);
+        $socket->setOption(SOL_SOCKET,SO_REUSEPORT,true);
+        $socket->setOption(SOL_SOCKET,SO_REUSEADDR,true);
+        $ret = $socket->bind($arg->getListenAddress(),$arg->getListenPort());
+        $ret = $socket->listen(2048);
     }
 
     public function onShutDown()
