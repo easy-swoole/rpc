b727df69704526f12611f8d9f9c8b1e0bae8fca1
yf <291323003@qq.com>
2019-06-01 11:53:00 +0800

    up fix

--- src/NodeManager/RedisManager.php
+++ src/NodeManager/RedisManager.php
@@ -4,32 +4,57 @@
 namespace EasySwoole\Rpc\NodeManager;
 
 
+use EasySwoole\Component\Pool\PoolConf;
+use EasySwoole\Component\Pool\PoolManager;
 use EasySwoole\Rpc\ServiceNode;
+use Swoole\Coroutine\Channel;
+use Swoole\Runtime;
 
 class RedisManager implements NodeManagerInterface
 {
-    function __construct(string $host,$port = 6379,$auth = null)
+    protected $redisKey;
+    /** @var Channel */
+    protected $channel;
+    function __construct(string $host,$port = 6379,$auth = null,string $hashKey = '__rpcNodes',int $maxRedisNum = 10)
     {
+        $this->redisKey = $hashKey;
+        Runtime::enableCoroutine();
+        PoolManager::getInstance()->registerAnonymous('__rpcRedis',function (PoolConf $conf)use($host,$port,$auth,$maxRedisNum){
+            $conf->setMaxObjectNum($maxRedisNum);
+            $redis = new \Redis();
+            $redis->connect($host,$port);
+            if($auth){
+                $redis->auth($auth);
+            }
+            $redis->setOption(\Redis::OPT_SERIALIZER,\Redis::SERIALIZER_PHP);
+            return $redis;
+        });
     }
 
     function getServiceNodes(string $serviceName, ?string $version = null): array
     {
-        // TODO: Implement getServiceNodes() method.
+        $redis = PoolManager::getInstance()->getPool('__rpcRedis')->getObj(15);
+        try{
+            $nodes = $redis->hGet($this->redisKey,$serviceName);
+        }catch (\Throwable $throwable){
+            //如果该redis断线则销毁
+            PoolManager::getInstance()->getPool('__rpcRedis')->unsetObj($redis);
+        }
     }
 
     function getServiceNode(string $serviceName, ?string $version = null): ?ServiceNode
     {
-        // TODO: Implement getServiceNode() method.
+
     }
 
     function allServiceNodes(): array
     {
-        // TODO: Implement allServiceNodes() method.
+
     }
 
     function deleteServiceNode(ServiceNode $serviceNode): bool
     {
-        // TODO: Implement deleteServiceNode() method.
+
     }
 
     function serviceNodeHeartBeat(ServiceNode $serviceNode): bool
