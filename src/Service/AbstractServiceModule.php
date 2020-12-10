8082d451f931c612315606f18930ed0f2d4d6458
如果的如果 <admin@fosuss.com>
2020-12-10 16:58:41 +0800

    up fix

--- src/Service/AbstractServiceModule.php
+++ src/Service/AbstractServiceModule.php
@@ -4,7 +4,30 @@
 namespace EasySwoole\Rpc\Service;
 
 
+use EasySwoole\Rpc\NetWork\Request;
+use EasySwoole\Rpc\NetWork\Response;
+
 abstract class AbstractServiceModule
 {
     abstract function moduleName():string;
+
+    protected function onRequest(Request $request):bool
+    {
+        return true;
+    }
+
+    protected function onException(\Throwable $throwable)
+    {
+
+    }
+
+    protected function onActionNotFound(Request $request)
+    {
+
+    }
+
+    public function __exec(Request $request,Response $response)
+    {
+
+    }
 }
